ecf8557454675b98b601314dd3064bb499dfb90f
zhaogezhang <18341265598@163.com>
2019-12-30 19:22:24 +0800

    Add comments about snmp trap.

--- src/apps/snmp/snmp_traps.c
+++ src/apps/snmp/snmp_traps.c
@@ -51,6 +51,7 @@
 #include "snmp_asn1.h"
 #include "snmp_core_priv.h"
 
+/* 保存了系统需要发送的 trap 数据包数据 */
 struct snmp_msg_trap {
   /* source enterprise ID (sysObjectID) */
   const struct snmp_obj_id *enterprise;
@@ -81,6 +82,7 @@ static u16_t snmp_trap_header_sum(struct snmp_msg_trap *trap, u16_t vb_len);
 static err_t snmp_trap_header_enc(struct snmp_msg_trap *trap, struct snmp_pbuf_stream *pbuf_stream);
 static err_t snmp_trap_varbind_enc(struct snmp_msg_trap *trap, struct snmp_pbuf_stream *pbuf_stream, struct snmp_varbind *varbinds);
 
+/* 如果指定的状态码 code != ERR_OK 则打印 log 信息并返回 ERR_ARG 值 */
 #define BUILD_EXEC(code) \
   if ((code) != ERR_OK) { \
     LWIP_DEBUGF(SNMP_DEBUG, ("SNMP error during creation of outbound trap frame!")); \
@@ -109,6 +111,15 @@ static u8_t snmp_auth_traps_enabled = 0;
  * @param dst_idx index in 0 .. SNMP_TRAP_DESTINATIONS-1
  * @param enable switch if 0 destination is disabled >0 enabled.
  */
+/*********************************************************************************************************
+** 函数名称: snmp_trap_dst_enable
+** 功能描述: 设置 trap_dst 数组指定索引位置处的地址使能情况
+** 输	 入: dst_idx - 指定的 trap_dst 数组的索引值
+**         : enable - 设置使能情况，0 表示失能，>0 表示使能
+** 输	 出: 
+** 全局变量: 
+** 调用模块: 
+*********************************************************************************************************/
 void
 snmp_trap_dst_enable(u8_t dst_idx, u8_t enable)
 {
@@ -124,6 +135,15 @@ snmp_trap_dst_enable(u8_t dst_idx, u8_t enable)
  * @param dst_idx index in 0 .. SNMP_TRAP_DESTINATIONS-1
  * @param dst IPv4 address in host order.
  */
+/*********************************************************************************************************
+** 函数名称: snmp_trap_dst_ip_set
+** 功能描述: 设置 trap_dst 数组指定索引位置处的目的 IP 地址信息
+** 输	 入: dst_idx - 指定的 trap_dst 数组的索引值
+**         : dst - 指定的目的 IP 地址信息
+** 输	 出: 
+** 全局变量: 
+** 调用模块: 
+*********************************************************************************************************/
 void
 snmp_trap_dst_ip_set(u8_t dst_idx, const ip_addr_t *dst)
 {
@@ -137,6 +157,15 @@ snmp_trap_dst_ip_set(u8_t dst_idx, const ip_addr_t *dst)
  * @ingroup snmp_traps
  * Enable/disable authentication traps
  */
+/*********************************************************************************************************
+** 函数名称: snmp_set_auth_traps_enabled
+** 功能描述: 设置 trap_dst 数组指定索引位置处的目的 IP 地址信息
+** 输	 入: dst_idx - 指定的 trap_dst 数组的索引值
+**         : dst - 指定的目的 IP 地址信息
+** 输	 出: 
+** 全局变量: 
+** 调用模块: 
+*********************************************************************************************************/
 void
 snmp_set_auth_traps_enabled(u8_t enable)
 {
@@ -147,6 +176,14 @@ snmp_set_auth_traps_enabled(u8_t enable)
  * @ingroup snmp_traps
  * Get authentication traps enabled state
  */
+/*********************************************************************************************************
+** 函数名称: snmp_get_auth_traps_enabled
+** 功能描述: 获取当前系统有关 auth traps 的使能情况
+** 输	 入: 
+** 输	 出: 
+** 全局变量: 
+** 调用模块: 
+*********************************************************************************************************/
 u8_t
 snmp_get_auth_traps_enabled(void)
 {
@@ -170,6 +207,18 @@ snmp_get_auth_traps_enabled(void)
  * and .iso.org.dod.internet.private.enterprises.yourenterprise
  * (sysObjectID) for specific traps.
  */
+/*********************************************************************************************************
+** 函数名称: snmp_send_trap
+** 功能描述: 根据函数指定的参数创建一个 trap 数据包并发送到 trap_dst 数组中的每一个有效地址处
+** 输	 入: eoid - 指定的企业 oid 数据
+**         : generic_trap - 指定的 trap 码
+**         : specific_trap - 当 generic_trap == 6 时用于企业级 trap
+**         : varbinds - 需要发送的 variable bind 列表
+** 输	 出: ERR_OK - 发送成功
+**         : ERR_MEM - 内存错误
+** 全局变量: 
+** 调用模块: 
+*********************************************************************************************************/
 err_t
 snmp_send_trap(const struct snmp_obj_id *eoid, s32_t generic_trap, s32_t specific_trap, struct snmp_varbind *varbinds)
 {
@@ -183,16 +232,24 @@ snmp_send_trap(const struct snmp_obj_id *eoid, s32_t generic_trap, s32_t specifi
 
   trap_msg.snmp_version = 0;
 
+  /* 遍历当前 trap_dst 数组中的每一个数组成员结构 */
   for (i = 0, td = &trap_dst[0]; i < SNMP_TRAP_DESTINATIONS; i++, td++) {
+
+    /* 判断当前遍历的地址信息是否使能并有效 */
     if ((td->enable != 0) && !ip_addr_isany(&td->dip)) {
+		
       /* lookup current source address for this dst */
+	  /* 为指定的目的地 IP 地址在当前协议栈中查找一个用于发送这个数据包的网路接口的 IP 地址 */
       if (snmp_get_local_ip_for_dst(snmp_traps_handle, &td->dip, &trap_msg.sip)) {
+
+	    /* 如果当前函数没有指定企业 oid 数据则使用当前系统默认的企业 oid */
         if (eoid == NULL) {
           trap_msg.enterprise = snmp_get_device_enterprise_oid();
         } else {
           trap_msg.enterprise = eoid;
         }
 
+        /* 设置当前 trap 数据包的 trap 码信息 */
         trap_msg.gen_trap = generic_trap;
         if (generic_trap == SNMP_GENTRAP_ENTERPRISE_SPECIFIC) {
           trap_msg.spc_trap = specific_trap;
@@ -200,28 +257,43 @@ snmp_send_trap(const struct snmp_obj_id *eoid, s32_t generic_trap, s32_t specifi
           trap_msg.spc_trap = 0;
         }
 
+        /* 设置当前 trap 数据包的时间戳信息 */
         MIB2_COPY_SYSUPTIME_TO(&trap_msg.ts);
 
         /* pass 0, calculate length fields */
+		/* 计算指定的 variable bind 数据列表转换成 tlv 结构时 length 字段占用几个字节 */
         tot_len = snmp_trap_varbind_sum(&trap_msg, varbinds);
+
+		/* 计算指定的 trap 数据结构转换成 tlv 结构时 length 字段占用几个字节 */
         tot_len = snmp_trap_header_sum(&trap_msg, tot_len);
 
         /* allocate pbuf(s) */
+		/* 为当前待发送的数据包申请一个 pbuf 数据结构 */
         p = pbuf_alloc(PBUF_TRANSPORT, tot_len, PBUF_RAM);
         if (p != NULL) {
+			
           struct snmp_pbuf_stream pbuf_stream;
+
+		  /* 根据函数指定参数初始化指定的 snmp 数据缓冲流结构 */
           snmp_pbuf_stream_init(&pbuf_stream, p, 0, tot_len);
 
           /* pass 1, encode packet into the pbuf(s) */
+		  /* 把指定的 trap header 的 asn1_tlv 结构参数按照 tlv 结构编码到指定的 snmp 数据缓冲流中 */
           snmp_trap_header_enc(&trap_msg, &pbuf_stream);
+
+		  /* 把指定的 trap varbind 的 asn1_tlv 结构参数按照 tlv 结构编码到指定的 snmp 数据缓冲流中 */
           snmp_trap_varbind_enc(&trap_msg, &pbuf_stream, varbinds);
 
           snmp_stats.outtraps++;
           snmp_stats.outpkts++;
 
           /** send to the TRAP destination */
+		  /* 把打包好的 trap 数据包发送到指定的 trap_dst 目的地址处 */
           snmp_sendto(snmp_traps_handle, p, &td->dip, LWIP_IANA_PORT_SNMP_TRAP);
+
+		  /* 数据包发送完成之后释放之前申请的 pbuf 缓冲区结构 */
           pbuf_free(p);
+		  
         } else {
           err = ERR_MEM;
         }
@@ -238,6 +310,15 @@ snmp_send_trap(const struct snmp_obj_id *eoid, s32_t generic_trap, s32_t specifi
  * @ingroup snmp_traps
  * Send generic SNMP trap
  */
+/*********************************************************************************************************
+** 函数名称: snmp_send_trap_generic
+** 功能描述: 根据函数指定的参数创建一个 generic trap 数据包并发送到 trap_dst 数组中的每一个有效地址处
+** 输	 入: generic_trap - 指定的 trap 码
+** 输	 出: ERR_OK - 发送成功
+**         : ERR_MEM - 内存错误
+** 全局变量: 
+** 调用模块: 
+*********************************************************************************************************/
 err_t
 snmp_send_trap_generic(s32_t generic_trap)
 {
@@ -249,6 +330,16 @@ snmp_send_trap_generic(s32_t generic_trap)
  * @ingroup snmp_traps
  * Send specific SNMP trap with variable bindings
  */
+/*********************************************************************************************************
+** 函数名称: snmp_send_trap_specific
+** 功能描述: 根据函数指定的参数创建一个 specific trap 数据包并发送到 trap_dst 数组中的每一个有效地址处
+** 输	 入: specific_trap - 指定的 specific trap 码
+**         : varbinds - 需要发送的 variable bind 列表
+** 输	 出: ERR_OK - 发送成功
+**         : ERR_MEM - 内存错误
+** 全局变量: 
+** 调用模块: 
+*********************************************************************************************************/
 err_t
 snmp_send_trap_specific(s32_t specific_trap, struct snmp_varbind *varbinds)
 {
@@ -259,6 +350,15 @@ snmp_send_trap_specific(s32_t specific_trap, struct snmp_varbind *varbinds)
  * @ingroup snmp_traps
  * Send coldstart trap
  */
+/*********************************************************************************************************
+** 函数名称: snmp_coldstart_trap
+** 功能描述: 创建一个 cold start trap 数据包并发送到 trap_dst 数组中的每一个有效地址处
+** 输	 入: 
+** 输	 出: ERR_OK - 发送成功
+**         : ERR_MEM - 内存错误
+** 全局变量: 
+** 调用模块: 
+*********************************************************************************************************/
 void
 snmp_coldstart_trap(void)
 {
@@ -269,6 +369,15 @@ snmp_coldstart_trap(void)
  * @ingroup snmp_traps
  * Send authentication failure trap (used internally by agent)
  */
+/*********************************************************************************************************
+** 函数名称: snmp_authfail_trap
+** 功能描述: 创建一个 authentication failure trap 数据包并发送到 trap_dst 数组中的每一个有效地址处
+** 输	 入: 
+** 输	 出: ERR_OK - 发送成功
+**         : ERR_MEM - 内存错误
+** 全局变量: 
+** 调用模块: 
+*********************************************************************************************************/
 void
 snmp_authfail_trap(void)
 {
@@ -277,6 +386,15 @@ snmp_authfail_trap(void)
   }
 }
 
+/*********************************************************************************************************
+** 函数名称: snmp_trap_varbind_sum
+** 功能描述: 计算指定的 variable bind 数据列表转换成 tlv 结构时 length 字段占用几个字节
+** 输	 入: trap - 指定的 trap 数据包指针
+**         : varbinds - 指定的 variable bind 数据列表指针
+** 输	 出: tot_len - 表示指定的 trap 数据包转换成 tlv 结构时 length 字段占用几个字节
+** 全局变量: 
+** 调用模块: 
+*********************************************************************************************************/
 static u16_t
 snmp_trap_varbind_sum(struct snmp_msg_trap *trap, struct snmp_varbind *varbinds)
 {
@@ -286,9 +404,12 @@ snmp_trap_varbind_sum(struct snmp_msg_trap *trap, struct snmp_varbind *varbinds)
 
   tot_len = 0;
   varbind = varbinds;
+
+  /* 分别遍历 varbinds 列表中的每一个 variable bind 并统计把他们转换成 tlv 结构时 length 字段占用几个字节 */
   while (varbind != NULL) {
     struct snmp_varbind_len len;
 
+    /* 计算指定的 snmp variable bind 结构相关的长度信息 */
     if (snmp_varbind_length(varbind, &len) == ERR_OK) {
       tot_len += 1 + len.vb_len_len + len.vb_value_len;
     }
@@ -297,6 +418,8 @@ snmp_trap_varbind_sum(struct snmp_msg_trap *trap, struct snmp_varbind *varbinds)
   }
 
   trap->vbseqlen = tot_len;
+
+  /* 计算指定 unsigned 16 bit 变量值转换成 tlv 结构时 length 字段占用几个字节 */
   snmp_asn1_enc_length_cnt(trap->vbseqlen, &tot_len_len);
   tot_len += 1 + tot_len_len;
 
@@ -311,6 +434,15 @@ snmp_trap_varbind_sum(struct snmp_msg_trap *trap, struct snmp_varbind *varbinds)
  * @param vb_len varbind-list length
  * @return the required length for encoding the trap header
  */
+/*********************************************************************************************************
+** 函数名称: snmp_trap_header_sum
+** 功能描述: 计算指定的 trap 数据结构转换成 tlv 结构时 length 字段占用几个字节
+** 输	 入: trap - 指定的 trap 数据包指针
+**         : vb_len - 指定的 variable bind 数据转换成 tlv 结构时 length 字段占用字节数
+** 输	 出: tot_len - 表示指定的 trap header 数据结构转换成 tlv 结构时 length 字段占用几个字节
+** 全局变量: 
+** 调用模块: 
+*********************************************************************************************************/
 static u16_t
 snmp_trap_header_sum(struct snmp_msg_trap *trap, u16_t vb_len)
 {
@@ -367,6 +499,17 @@ snmp_trap_header_sum(struct snmp_msg_trap *trap, u16_t vb_len)
   return tot_len;
 }
 
+/*********************************************************************************************************
+** 函数名称: snmp_trap_varbind_enc
+** 功能描述: 把指定的 trap varbind 的 asn1_tlv 结构参数按照 tlv 结构编码到指定的 snmp 数据缓冲流中
+** 输	 入: trap - 指定的 trap 数据包指针
+**         : varbinds - 指定的 varbind 的 asn1_tlv 结构参数
+** 输	 出: pbuf_stream - 用来存储编码后数据的 snmp 数据缓冲流指针
+**         : ERR_OK - 编码成功
+**         : ERR_ARG - 编码错误
+** 全局变量: 
+** 调用模块: 
+*********************************************************************************************************/
 static err_t
 snmp_trap_varbind_enc(struct snmp_msg_trap *trap, struct snmp_pbuf_stream *pbuf_stream, struct snmp_varbind *varbinds)
 {
@@ -374,11 +517,16 @@ snmp_trap_varbind_enc(struct snmp_msg_trap *trap, struct snmp_pbuf_stream *pbuf_
   struct snmp_varbind *varbind;
 
   varbind = varbinds;
-
+  
+  /* 根据指定参数初始化指定的 tlv 数据结构 */
   SNMP_ASN1_SET_TLV_PARAMS(tlv, SNMP_ASN1_TYPE_SEQUENCE, 0, trap->vbseqlen);
+
   BUILD_EXEC( snmp_ans1_enc_tlv(pbuf_stream, &tlv) );
 
   while (varbind != NULL) {
+
+    /* 把指定的 varbind 结构中的有用数据（oid、type、value_len、value）按照 ans1 格式编码
+       到指定的 snmp 数据缓冲流数据结构中 */
     BUILD_EXEC( snmp_append_outbound_varbind(pbuf_stream, varbind) );
 
     varbind = varbind->next;
@@ -390,6 +538,16 @@ snmp_trap_varbind_enc(struct snmp_msg_trap *trap, struct snmp_pbuf_stream *pbuf_
 /**
  * Encodes trap header from head to tail.
  */
+/*********************************************************************************************************
+** 函数名称: snmp_trap_header_enc
+** 功能描述: 把指定的 trap header 的 asn1_tlv 结构参数按照 tlv 结构编码到指定的 snmp 数据缓冲流中
+** 输	 入: trap - 指定的 trap 数据包指针
+** 输	 出: pbuf_stream - 用来存储编码后数据的 snmp 数据缓冲流指针
+**         : ERR_OK - 编码成功
+**         : ERR_ARG - 编码错误
+** 全局变量: 
+** 调用模块: 
+*********************************************************************************************************/
 static err_t
 snmp_trap_header_enc(struct snmp_msg_trap *trap, struct snmp_pbuf_stream *pbuf_stream)
 {
